edb6335e685c139a647dbb6ed1b9f3bd51164266
Prima Aulia Gusta <primaulia@gmail.com>
2016-08-02 00:54:40 +0800

    allow cors access

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module RailsApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    allow do
-      origins '*'
-      resource '*', :headers => :any, :methods => [:get, :post, :options]
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
     end
   end
 end
